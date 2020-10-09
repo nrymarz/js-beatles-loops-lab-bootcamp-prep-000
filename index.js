54ead1cc871505987bbfb368b4bab2dc5b8ab0c7
Nathan Rymarz <nrymarz@gmail.com>
2020-10-09 23:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var arr = new Array();
   for(let i = 0; i<musicians.length;i++){
-    arr.push(`${musicians[i]} plays the ${instruments[i]}`)
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return arr;
 }
