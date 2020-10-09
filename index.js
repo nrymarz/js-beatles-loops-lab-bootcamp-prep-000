7278d7482d5a84472ae72c79e637524b285c6857
Nathan Rymarz <nrymarz@gmail.com>
2020-10-09 23:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr = new array();
+  for(let i = 0; i<musicians.length;i++){
+    arr.push(`${musicians[i]} plays the ${instruments[i]}`)
+  }
+  return arr;
+}
